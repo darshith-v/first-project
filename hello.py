0d8d35ad8811df589535c9aa2a738e741d0b783d
darshith-v <darshithv3392@gmail.com>
2023-02-26 20:08:18 +0530

    first turtle project

--- hello.py
+++ hello.py
@@ -1,1 +1,24 @@
-print("helloworld")
+import turtle
+wn = turtle.Screen()
+wn.bgcolor("olivegreen")
+alex = turtle.Turtle()
+alex.forward(50)
+alex.left(90)
+alex.forward(50)
+alex.left(90)
+alex.forward(50)
+alex.left(90)
+alex.forward(50)
+
+alex.left(180)
+alex.forward(100)
+
+jul = turtle.Turtle()
+jul.left(120)
+jul.forward(90)
+jul.left(120)
+jul.forward(90)
+jul.left(120)
+jul.forward(90)
+
+wn.exitonclick()
